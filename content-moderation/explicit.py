e442d918d541b489ab18bd7ee9eecdc6212fd54b
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2023-06-03 01:37:13 +0200

    safe-search

---
+++ content-moderation/explicit.py
@@ -0,0 +1,28 @@
+def detect_safe_search_uri(uri):
+    """Detects unsafe features in the file located in Google Cloud Storage or
+    on the Web."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = uri
+
+    response = client.safe_search_detection(image=image)
+    safe = response.safe_search_annotation
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    print('Safe search:')
+
+    print(f'adult: {likelihood_name[safe.adult]}')
+    print(f'medical: {likelihood_name[safe.medical]}')
+    print(f'spoofed: {likelihood_name[safe.spoof]}')
+    print(f'violence: {likelihood_name[safe.violence]}')
+    print(f'racy: {likelihood_name[safe.racy]}')
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+        
